cfb52589b6fdcc85c043b88b61ad1fd1afd25d23
Hussein <hussein.abdirahman14@gmail.com>
2024-08-06 11:18:28 -0700

    Added all files in text directory

---
+++ firstc.c
@@ -0,0 +1,20 @@
+/* https://www.codingunit.com/c-tutorial-first-c-program-hello-world
+
+	Description	: Print Hello World on the screen
+
+		Author		: Your name
+
+		Date		: 01/01/2007			
+                Here is how to compile it
+https://www.geeksforgeeks.org/compiling-a-c-program-behind-the-scenes */
+#include<stdio.h>
+
+	int main()
+
+	{
+
+		printf("Hello World \n");
+
+		return 0;
+
+	}
